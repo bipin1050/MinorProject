6100f0eb99b7eec1ea0645bcb43c38730ef374b0
bipin1050 <bipin.khanal1050@gmail.com>
2022-01-29 00:00:07 +0545

    last push of jan 28

--- src/components/addproduct.js
+++ src/components/addproduct.js
@@ -1,7 +1,32 @@
 const Addproduct = () => {
     return ( 
-        <div>
-            Add Products
+        <div className="bg-neutral-300">
+            <form>
+                <div className="container mx-auto">
+                    <div className="container mx-auto w-1/4 py-6 text-2xl text-center">Enter New Product</div>
+                    <div className="container mx-auto w-1/4">
+                        <input type ="text" placeholder="Product Name" className="h-9 w-full rounded-lg border-black"/><br/><br/>
+                        <input type ="text" placeholder="Cateegory" className="h-9 w-full rounded-lg"/><br/><br/>
+                        <input type ="text" placeholder="Manufaturer" className="h-9 w-full rounded-lg"/><br/><br/>
+                        <input type ="text" placeholder="Batch Number" className="h-9 w-full rounded-lg"/><br/><br />
+                        <label>Manufaturer Date</label><br/>
+                        <input type ="date" placeholder="mm/dd/yyyy" className="h-9 w-full rounded-lg"/><br/><br/>
+                        <label>Entry Date</label><br/>
+                        <input type ="date" placeholder="mm/dd/yyyy" className="h-9 w-full rounded-lg"/><br/><br/>
+                        <label>Expiry Date</label><br/>
+                        <input type ="date" placeholder="mm/dd/yyyy" className="h-9 w-full rounded-lg"/><br/><br/>
+                        <input type ="text" placeholder="Unit Price" className="h-9 w-full rounded-lg"/><br/><br/>
+                        <input type ="text" placeholder="Quantity" className="h-9 w-full rounded-lg"/><br/><br/>
+                        <input type ="text" placeholder="Target" className="h-9 w-full rounded-lg"/><br/><br/>
+
+                        <div className="text-center">
+                            <button className="border-4 px-10 py-2 mb-10 rounded-2xl bg-green-700 hover:bg-stone-50 hover:text-black">
+                                Add
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </form>
         </div>
      );
 }
